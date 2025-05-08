96d64c1cc2d23cc4904c7ee41f7876333ea38951
Aadit Sule <aaditsule0605@gmail.com>
2025-05-08 15:58:36 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     description="A sample Python package",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
-    url="https://github.com/yourusername/simpleprinter",
+    url="https://github.com/aaditsule/python_package",
     packages=find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
